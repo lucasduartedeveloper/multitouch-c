aba91b4d6fa9f8a6af2bfd6742984ac683a53ab8
Lucas <lucasduartedeveloper@gmail.com>
2023-10-05 15:51:36 -0300

    first commit

--- script/line-view.js
+++ script/line-view.js
@@ -47,6 +47,24 @@ $(document).ready(function() {
     canvas.style.zIndex = "11";
     document.body.appendChild(canvas);
 
+    infoView = document.createElement("span");
+    infoView.style.position = "absolute";
+    infoView.style.color = "#fff";
+    infoView.style.fontWeight = "900";
+    infoView.innerText = "60 10";
+    infoView.style.lineHeight = "50px";
+    infoView.style.fontSize = "15px";
+    infoView.style.textAlign = "left";
+    infoView.style.fontFamily = "Khand";
+    infoView.style.left = (10)+"px";
+    infoView.style.top = (10)+"px";
+    infoView.style.width = (100)+"px";
+    infoView.style.height = (50)+"px"; 
+    infoView.style.scale = "0.9";
+    infoView.style.borderRadius= "25px";
+    infoView.style.zIndex = "15";
+    document.body.appendChild(infoView);
+
     toggleView = document.createElement("span");
     toggleView.style.position = "absolute";
     toggleView.style.background = backgroundColor;
@@ -68,8 +86,98 @@ $(document).ready(function() {
     toggleView.style.zIndex = "15";
     document.body.appendChild(toggleView);
 
-    toggleView.onclick = function() {
-       updateImage = !updateImage;
+    deviceList = [ "front", "back", "back1", "back2", "back3" ];
+    deviceView = document.createElement("span");
+    deviceView.style.position = "absolute";
+    deviceView.style.background = "#fff";
+    deviceView.style.color = "#000";
+    deviceView.style.fontWeight = "900";
+    deviceView.innerText = deviceList[deviceNo];
+    deviceView.style.lineHeight = "50px";
+    deviceView.style.fontSize = "15px";
+    deviceView.style.textAlign = "center";
+    deviceView.style.fontFamily = "Khand";
+    deviceView.style.left = (110)+"px";
+    deviceView.style.top = (sh-60)+"px";
+    deviceView.style.width = (50)+"px";
+    deviceView.style.height = (50)+"px"; 
+    deviceView.style.scale = "0.9";
+    deviceView.style.border = "1px solid #000"; 
+    deviceView.style.borderRadius= "25px";
+    deviceView.style.zIndex = "15";
+    document.body.appendChild(deviceView);
+
+    deviceView.onclick = function() {
+        deviceNo = (deviceNo+1) < videoDevices.length ? 
+        (deviceNo+1) : 0;
+
+        camera.style.transform = (deviceNo == 0) ? 
+        "rotateY(-180deg)" : "initial";
+        deviceView.innerText = deviceList[deviceNo];
+    };
+
+    updateCircle = true;
+    toggleCircleView = document.createElement("span");
+    toggleCircleView.style.position = "absolute";
+    toggleCircleView.style.background = "#fff";
+    toggleCircleView.style.color = "#000";
+    toggleCircleView.style.fontWeight = "900";
+    toggleCircleView.style.lineHeight = "50px";
+    toggleCircleView.style.fontSize = "15px";
+    toggleCircleView.style.textAlign = "center";
+    toggleCircleView.innerText = "ON";
+    toggleCircleView.style.fontFamily = "Khand";
+    toggleCircleView.style.left = (160)+"px";
+    toggleCircleView.style.top = (sh-60)+"px";
+    toggleCircleView.style.width = (50)+"px";
+    toggleCircleView.style.height = (50)+"px"; 
+    toggleCircleView.style.scale = "0.9";
+    toggleCircleView.style.border = "1px solid #000"; 
+    toggleCircleView.style.borderRadius= "25px";
+    toggleCircleView.style.zIndex = "15";
+    document.body.appendChild(toggleCircleView);
+
+    toggleCircleView.onclick = function() {
+        updateCircle = !updateCircle;
+        toggleCircleView.innerText = updateCircle ? "ON" : "OFF";
+    };
+
+    airResistanceLabel = document.createElement("span");
+    airResistanceLabel.style.position = "absolute";
+    airResistanceLabel.style.color = "#fff";
+    //airResistanceLabel.style.fontWeight = "900";
+    airResistanceLabel.style.lineHeight = "25px";
+    airResistanceLabel.style.fontSize = "15px";
+    airResistanceLabel.style.textAlign = "center";
+    airResistanceLabel.innerText = "Air resistance: 0";
+    airResistanceLabel.style.fontFamily = "Khand";
+    airResistanceLabel.style.left = (220)+"px";
+    airResistanceLabel.style.top = (sh-60)+"px";
+    airResistanceLabel.style.width = (100)+"px";
+    airResistanceLabel.style.height = (25)+"px"; 
+    airResistanceLabel.style.scale = "0.9";
+    airResistanceLabel.style.zIndex = "15";
+    document.body.appendChild(airResistanceLabel);
+
+    airResistanceView = document.createElement("input");
+    airResistanceView.style.position = "absolute";
+    airResistanceView.max = 5;
+    airResistanceView.min = -5;
+    airResistanceView.step = 0.1;
+    airResistanceView.value = 0;
+    airResistanceView.type = "range";
+    airResistanceView.style.left = (220)+"px";
+    airResistanceView.style.top = (sh-35)+"px";
+    airResistanceView.style.width = (100)+"px";
+    airResistanceView.style.height = (25)+"px"; 
+    airResistanceView.style.scale = "0.9";
+    airResistanceView.style.zIndex = "15";
+    document.body.appendChild(airResistanceView);
+
+    airResistanceView.onchange = function() {
+        airResistance = airResistanceView.value;
+        airResistanceLabel.innerText = "Air resistance: "+
+        airResistanceView.value;
     };
 
     startTime = 0;
@@ -83,21 +191,48 @@ $(document).ready(function() {
 
         readings += 1;
         if (new Date().getTime() - startTime > 1000) {
-            console.log("updated "+readings+" times");
+            updateInfo(lastFrameCount, readings);
+            lastReadings = readings;
             readings = 0;
             startTime = new Date().getTime();
         }
-        position.x += -(gyro.accX);
+
+        var accX = -(gyro.accX)+airResistance;
+
+        if (updateImage) {
+            position.x += accX;
+            if (position.x < (size/2))
+            position.x = (size/2);
+            if (position.x > sw-(size/2))
+            position.x = sw-(size/2);
+        }
     };
 
     animate();
 });
 
+var airResistance = 0;
+
+var lastReadings = 0;
+var lastFrameCount = 0;
+var updateInfo = function(frameCount, readings) {
+    infoView.innerText = frameCount + " " + readings;
+};
+
+var frameCount = 0;
+var renderTime = 0;
 var updateImage = true;
 var position = { x: (sw/2), y: (sh/2) };
 var animate = function() {
     if (updateImage) {
         drawImage();
+    }
+    frameCount += 1;
+    if (new Date().getTime() - renderTime > 1000) {
+        updateInfo(frameCount, lastReadings);
+        lastFrameCount = frameCount;
+        frameCount = 0;
+        renderTime = new Date().getTime();
     }
     requestAnimationFrame(animate);
 };
@@ -108,7 +243,7 @@ var drawImage = function() {
      ctx.clearRect(0, 0, sw, sh);
 
      ctx.lineWidth = 1;
-     ctx.strokeStyle = "#fff";
+     ctx.strokeStyle = "#000";
 
      ctx.beginPath();
      ctx.arc(position.x, position.y, (size/2), 0, (Math.PI*2));
@@ -125,4 +260,44 @@ var drawImage = function() {
      ctx.moveTo(sw, (sh/2));
      ctx.lineTo(end1, (sh/2));
      ctx.stroke();
+
+     var clip = document.createElement("canvas");
+     clip.width = size;
+     clip.height = size;
+
+     var clipCtx = clip.getContext("2d");
+
+     if (cameraOn) {
+         clipCtx.save();
+         if (deviceNo == 0) {
+             clipCtx.scale(-1, 1);
+             clipCtx.translate(-size, 0);
+         }
+
+         var offsetX = (vw-sw)/2;
+         var offsetY = (vh-sh)/2;
+
+         var format = {
+             left: (vw/2)-(size/2),
+             top: (vh/2)-(size/2),
+             width: size,
+             height: size
+         };
+
+         clipCtx.drawImage(camera, 
+         format.left, format.top, 
+         format.width, format.height,
+         0, 0, size, size);
+         clipCtx.restore();
+
+         ctx.save();
+         ctx.beginPath();
+         ctx.arc(position.x, position.y, (size/2), 0, (Math.PI*2));
+         ctx.clip();
+
+         if (updateCircle)
+         ctx.drawImage(clip, 
+         position.x-(size/2), position.y-(size/2), size, size);
+         ctx.restore();
+     }
 };
